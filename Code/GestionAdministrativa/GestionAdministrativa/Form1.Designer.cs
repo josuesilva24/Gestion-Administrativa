9310b27677bc3718e2d718b86e53f5615de36086
Josue Silva <josuesilva24@gmail.com>
2018-01-27 15:46:32 -0600

    BD y forms

--- Code/GestionAdministrativa/GestionAdministrativa/Form1.Designer.cs
+++ Code/GestionAdministrativa/GestionAdministrativa/Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.configuracionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gestionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tiposDeCambioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configuracionToolStripMenuItem,
+            this.gestionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1137, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // configuracionToolStripMenuItem
+            // 
+            this.configuracionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tiposDeCambioToolStripMenuItem});
+            this.configuracionToolStripMenuItem.Name = "configuracionToolStripMenuItem";
+            this.configuracionToolStripMenuItem.Size = new System.Drawing.Size(114, 24);
+            this.configuracionToolStripMenuItem.Text = "Configuracion";
+            // 
+            // gestionToolStripMenuItem
+            // 
+            this.gestionToolStripMenuItem.Name = "gestionToolStripMenuItem";
+            this.gestionToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
+            this.gestionToolStripMenuItem.Text = "Gestion";
+            // 
+            // tiposDeCambioToolStripMenuItem
+            // 
+            this.tiposDeCambioToolStripMenuItem.Name = "tiposDeCambioToolStripMenuItem";
+            this.tiposDeCambioToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
+            this.tiposDeCambioToolStripMenuItem.Text = "Tipos De Cambio";
+            this.tiposDeCambioToolStripMenuItem.Click += new System.EventHandler(this.tiposDeCambioToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1137, 498);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestion Administrativa";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem configuracionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tiposDeCambioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gestionToolStripMenuItem;
     }
 }
 
--- Code/GestionAdministrativa/GestionAdministrativa/TiposDeCambiosForm.cs
+++ Code/GestionAdministrativa/GestionAdministrativa/TiposDeCambiosForm.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace GestionAdministrativa
 {
-    public partial class Form1 : Form
+    public partial class TiposDeCambiosForm : Form
     {
-        public Form1()
+        public TiposDeCambiosForm()
         {
             InitializeComponent();
-        }
-
-        private void tiposDeCambioToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            new TiposDeCambiosForm().Show();
         }
     }
 }
