988166b53fe59a23763e2055af1ff3fb323018a5
Josue Silva <josuesilva24@gmail.com>
2018-01-31 21:22:11 -0600

    otro

---
+++ Code/GestionAdministrativa/GestionAdministrativa/Proveedor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using MappingClasses;
+using System.Linq;
+
+namespace GestionAdministrativa
+{
+    public partial class ProveedorForm : Form
+    {
+        Proveedor TipoCambio = new Proveedor();
+        public ProveedorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TiposDeCambiosForm_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = TipoCambio.GetAllProveedores().
+                Select( x => new
+                {
+                    Id = x.Id,
+                    Nombre = x.Nombre,
+                    Identificacion = x.Identificacion,
+                    TelefonoPrincipal = x.Telefono,
+                    TelefonoSecundario = x.Telefono2,
+                    Notas = x.Notas
+                }).ToList();
+            
+        }
+    }
+}
